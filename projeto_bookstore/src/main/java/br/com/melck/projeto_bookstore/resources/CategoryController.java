7c0c9807416851e57796c702ddfa7821d73436ef
melck <melck_junior@hotmail.com>
2022-06-22 22:41:50 +0100

    Criação POST Category

--- projeto_bookstore/src/main/java/br/com/melck/projeto_bookstore/resources/CategoryController.java
+++ projeto_bookstore/src/main/java/br/com/melck/projeto_bookstore/resources/CategoryController.java
@@ -1,15 +1,18 @@
 package br.com.melck.projeto_bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.melck.projeto_bookstore.entities.Category;
 import br.com.melck.projeto_bookstore.resources.dtos.CategoryDTO;
@@ -35,6 +38,12 @@ public class CategoryController {
         .map(obj -> new CategoryDTO(obj))
         .collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
-       
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> create(@RequestBody Category obj){
+        obj = categoryService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+      return ResponseEntity.created(uri).build(); 
     }
 }
